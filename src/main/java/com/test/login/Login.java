a574072f3a3db9bf596ad5a1040309d1bf963f77
admin <admin@admin.com>
2016-07-22 06:52:11 -0500

    se ajusta vista

--- src/main/java/com/test/login/Login.java
+++ src/main/java/com/test/login/Login.java
@@ -3,10 +3,43 @@
  */
 package com.test.login;
 
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author USUARIO
  *
  */
-public class Login {
+public class Login implements Filter{
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		final String contextPath = ((HttpServletRequest) request)
+				.getContextPath();
+		((HttpServletResponse) response).sendRedirect(contextPath
+				+ "index.xhtml");
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
